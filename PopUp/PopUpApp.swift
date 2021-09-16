e9fb96f645d7d209ddda9a154e90854132f518dd
bullinnyc <i8u11@icloud.com>
2021-09-16 11:31:42 +0700

    Initial Commit

---
+++ PopUp/PopUpApp.swift
@@ -0,0 +1,18 @@
+//
+//  PopUpApp.swift
+//  PopUp
+//
+//  Created by Dmitry Kononchuk on 16.09.2021.
+//  Copyright © 2021 Dmitry Kononchuk. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct PopUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
